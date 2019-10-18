fea68bf6b644ffaed0f89593a1b02466d23142bd
wuqijia <virus578@126.com>
2019-10-18 14:14:03 +0800

    test

---
+++ src/slot/study/index.vue
@@ -0,0 +1,41 @@
+<template>
+  <div class=""/>
+</template>
+
+<script>
+export default {
+  name: '',
+  components: {
+
+  },
+  props: {
+  // 必须写默认值和数据类型
+  },
+
+  data() {
+    return {
+
+    }
+  },
+
+  computed: {
+
+  },
+
+  watch: {
+
+  },
+
+  mounted() {
+
+  },
+
+  methods: {
+
+  }
+}
+</script>
+
+<style lang="" scoped>
+
+</style>
